0d6427b48238cb762144c5e24a40ff4b865e2417
AA352872 <aa352872@AA352872-no-MacBook-Air.local>
2016-11-01 00:59:39 +0900

    created the test script

--- WEB-INF/src/bar/Calculator.java
+++ WEB-INF/src/bar/Calculator.java
@@ -5,5 +5,15 @@ public class Calculator {
 	public int multiply(int x, int y){
 		return x * y;
 	}
-
+	
+	public double divide(int x, int y){
+		
+		if(y == 0){
+			throw new IllegalArgumentException("divide by zero");
+		}
+		
+		return (double)x / (double)y;
+	}
+	
+	
 }
---
+++ test/CalculatorTest.java
@@ -0,0 +1,58 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import bar.Calculator;
+
+
+public class CalculatorTest {
+	
+	@Test
+	public void multiplyMethodTest3x4() {
+//		fail("Not yet implemented");
+		//SetUp
+		Calculator calc = new Calculator();
+		int expected = 12;
+		//Exercise
+		int actual = calc.multiply(3, 4);
+		//Verify
+		assertThat(actual, is(expected));
+		//tear down
+	}
+	
+	@Test
+	public void multiplyMethodTest5x7() {
+		//SetUp
+		Calculator calc = new Calculator();
+		int expected = 35;
+		//Exercise
+		int actual = calc.multiply(5, 7);
+		//Verify
+		assertThat(actual, is(expected));
+		//tear down
+	}
+	
+	@Test
+	public void divideMethodTest3waru2() {
+		//SetUp
+		Calculator calc = new Calculator();
+		double expected = 1.5;
+		//Exercise
+		double actual = calc.divide(3, 2);
+		//Verify
+		assertThat(actual, is(expected));
+		//tear down
+		
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void divideMethodTestwaru0() {
+		//SetUp
+		Calculator calc = new Calculator();
+		//Exercise
+		calc.divide(3, 0);
+		//Verify
+		//tear down
+	}
+	
+	
+}
