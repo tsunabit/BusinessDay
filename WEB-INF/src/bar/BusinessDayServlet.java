41c3de0867e2e926dc4b468d01e8b4a929704aca
your user name <tsuniki.miniki@gmail.com>
2016-11-20 04:00:09 +0900

    updated the validation logic

--- WEB-INF/src/bar/BusinessDayServlet.java
+++ WEB-INF/src/bar/BusinessDayServlet.java
@@ -30,6 +30,19 @@ public class BusinessDayServlet extends HttpServlet {
 		try {
 			Validation vali = new Validation();
 			//check the form input
+			if(!vali.checkNullString("startDate", request.getParameter("startDate"))){
+				request.setAttribute("InputError", "Start");
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/InputError.jsp");
+				dispatcher.forward(request, response);
+				return;
+			}else if(!vali.checkNullString("endDate", request.getParameter("endDate"))){
+				request.setAttribute("InputError", "End");
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/InputError.jsp");
+				dispatcher.forward(request, response);
+				return;
+			}
+			
+			
 			if(!vali.checkKaramoji(request.getParameter("startDate"))) {
 				request.setAttribute("InputError", "Start");
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/InputError.jsp");
@@ -42,11 +55,11 @@ public class BusinessDayServlet extends HttpServlet {
 				return;
 			}
 			
-			logger.trace("input startDate : " + LocalDate.parse(request.getParameter("startDate")));
-			logger.trace("input endDate   : " + LocalDate.parse(request.getParameter("endDate")));
 			//開始日と終了日をDate型へ変換する
 			LocalDate startDate = LocalDate.parse(request.getParameter("startDate"));
+			logger.trace("input startDate : " + LocalDate.parse(request.getParameter("startDate")));
 			LocalDate endDate = LocalDate.parse(request.getParameter("endDate"));
+			logger.trace("input endDate   : " + LocalDate.parse(request.getParameter("endDate")));
 			
 			int count = 0;
 			ArrayList<String> arrayWkd = new ArrayList<String>();
@@ -62,15 +75,17 @@ public class BusinessDayServlet extends HttpServlet {
 				}
 				startDate = startDate.plusDays(1);
 			}
-			logger.trace("Business day count = " + count);
+			
 			request.setAttribute("dayDiff", count);
+			logger.trace("Business day count = " + count);
 			
 			StringBuilder buff = new StringBuilder();
 			for(String skip : arrayWkd){
 				buff.append(skip + ",");
 			}
-			logger.trace("skip = " + buff.toString());
+			
 			request.setAttribute("arrayWkd", arrayWkd);
+			logger.trace("skip = " + buff.toString());
 			
 			//取得した日数の総時間を計算
 			Calculator cal = new Calculator();
--- WEB-INF/src/bar/Validation.java
+++ WEB-INF/src/bar/Validation.java
@@ -5,6 +5,14 @@ import org.apache.log4j.Logger;
 public class Validation {
 	final Logger logger = Logger.getLogger (Validation.class);
 	
+	public boolean checkNullString(String keyWord , String strInput){
+		if(strInput.equals("")) {
+			logger.error(keyWord + " is nullstring");
+			return false;
+		}
+		return true;
+	}
+	
 	public boolean checkKaramoji(String strInput) {
 		
 		if(strInput.equals("")) {
