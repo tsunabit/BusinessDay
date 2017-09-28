11474e6002f08dd5d50f63cf00858d3972a4309f
tsunabit <tsuniki.miniki@gmail.com>
2017-09-29 00:14:46 +0900

    JQuery UIを使って日付の入力制限を追加

--- script/MyScript.js
+++ script/MyScript.js
@@ -1,31 +1,35 @@
 /**
  * 
  */
-//console.log("script start");
+$(function() {
+	//jQuery UI Datepicker.JQuery UIのカレンダー機能.
+	$.datepicker.setDefaults({
+		//dateFormat : 'yy年mm月dd日',
+		dateFormat : 'yy-mm-dd',					//RFC3339形式の日付を指定
+		changeYear : true,
+		changeMonth: true,
+		showOn     : "both",						//テキストボックスの横にボタンを追加
+		buttonImage: 'jsp/img/calendar-icon.png',	//テキストボックス横のボタンの画像
+		buttonText : "カレンダーから選択",				//ツールチップ表示文言
+		buttonImageOnly: true						//単に画像として表示するか、ボタンとして表示しボタン内に画像を表示させるか
+	});
 
-//jQuery UI Datepicker.JQuery UIのカレンダー機能.
-$.datepicker.setDefaults({
-	//RFC3339形式の日付を指定
-	//dateFormat : 'yy年mm月dd日',
-	dateFormat : 'yy-mm-dd',
-	changeYear : true,
-	changeMonth: true,
-	showOn     : "both",						//テキストボックスの横にボタンを追加
-	buttonImage: 'jsp/img/calendar-icon.png',	//テキストボックス横のボタンの画像
-	buttonText : "カレンダーから選択",				//ツールチップ表示文言
-    buttonImageOnly: true						//単に画像として表示するか、ボタンとして表示しボタン内に画像を表示させるか
+	//jspの[id=startDate]にJQuery UIのdatepickerを設定
+	$('#startDate').datepicker({
+		onSelect: function (selectedDate) {
+			var option = 'minDate';
+			//datepickerのminDateオプションを設定
+            $('#endDate').datepicker('option', option, selectedDate);
+		}
+	});
+
+	//jspの[id=endDate]にJQuery UIのdatepickerを設定
+	$('#endDate').datepicker({
+		onSelect: function (selectedDate) {
+			var option = 'maxDate';
+			//datepickerのmaxDateオプションを設定
+            $('#startDate').datepicker('option', option, selectedDate);
+		}
+	});
 });
 
-$('#startDate').datepicker({
-	onSelect: function(startDate) {
-		$('#endDate').datepicker({
-			minDate:startDate
-		});
-	    //alert(startDate);
-	  }
-});
-
-$('#endDate').datepicker({
-});
-
-//console.log("script end");
