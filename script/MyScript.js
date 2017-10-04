7401d2d281267df5294640a8fb0c6a3d092f6d2a
tsunabit <tsuniki.miniki@gmail.com>
2017-10-05 00:34:21 +0900

    誤ったコメントを修正

--- script/MyScript.js
+++ script/MyScript.js
@@ -9,7 +9,7 @@ $(function() {
 		dateFormat : 'yy-mm-dd',					//RFC3339形式の日付を指定
 		changeYear : true,
 		changeMonth: true,
-		showOn     : "both",						//テキストボックスの横にボタンを追加
+		showOn     : "both",						//テキストボックス、カレンダーアイコンどちらをクリックしてもカレンダー表示
 		buttonImage: 'jsp/img/calendar-icon.png',	//テキストボックス横のボタンの画像
 		buttonText : "カレンダーから選択",				//ツールチップ表示文言
 		buttonImageOnly: true						//単に画像として表示するか、ボタンとして表示しボタン内に画像を表示させるか
